774c676b9b9e5ea80e22fe6e95f6b1f43ed06447
Hellysamar Araújo <hellysamar@hotmail.com>
2021-12-13 12:12:27 -0300

    adição do arquivo3, operações aritméticas

---
+++ 3-operacoesAritmeticas.js
@@ -0,0 +1,9 @@
+console.log("Operações Aritmétivas");
+
+console.log(2 + 2); //segue o mesmo padrão das operações matemáticas
+console.log(2 + 8 * 2); // mesmo em operações mais elaboradas, fazendo primeiro Multiplicação e Divisão para só depois a soma e subtração
+console.log((2 + 8) * 2); // e entre parenteses a primeira conta a ser feita
+
+console.log("ano" + 2021); // quando fazemos "conta" de uma string com um numero a operação não é matemática,
+
+console.log("2" + "2"); // mas sim uma concatenação, por isso as variaveis são juntadas/unidas ao inves de somadas
