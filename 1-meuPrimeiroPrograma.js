ffb6e2c402997d54e75107ca34ec6dcd8472df94
Hellysamar Araújo <hellysamar@hotmail.com>
2021-12-13 12:01:21 -0300

    alteração do arquivo1 e adição do arquivo2

--- 1-meuPrimeiroPrograma.js
+++ 1-meuPrimeiroPrograma.js
@@ -1,1 +1,11 @@
-console.log("Meu primeiro Programa em JavaScript");
+console.log("Meu primeiro Programa em JavaScript: trabalhando com variaveis");
+
+const idade;
+
+idade = 28;
+
+const idadeSomada = idade + 1;
+
+console.log ("idade"); // uma String pois está entre aspas "semelhante a Java"
+
+console.log (idade); // variavel, onde está armazenado o valor 281321
---
+++ 2-trabalhandoComVariaveis.js
@@ -0,0 +1,10 @@
+//Console.log("Trabalhando com variáveis");
+console.log ("Trabalhando com variáveis");
+// JS é CaseSensitive
+
+const idade = 29; // Variavel "i"dade
+const Idade = 30; // outra variável "I"dade por começar com letra maiúscula torna-se outra variável!
+
+// é boa pratica seguir um padrão de iniciar TODAS as variáveis com letra maiúscula, ou todas Minúsculas, mas nunca utilizar das 2 opções
+
+const nome = "Helly";
